3559de1d1955886de09128dde2525718d2766ab1
Alisa Prusa <aprusa@ucsc.edu>
2015-11-04 16:52:45 -0800

    Quote Submission Simple No Parse

--- gameQuotes/gameQuotes.js
+++ gameQuotes/gameQuotes.js
@@ -1,3 +1,21 @@
 //this is the parse javascript file for S.pace website
 //https://parse.com/tutorials/todo-app-with-javascript
 //learn parse from code academy
+var container = document.getElementById('container');
+
+void start(){
+     var submit = documents.getElementsById('submit');
+     var quote = documents.getElementsById('quote');
+     if(quote.value == 'temp'){
+          var success = createElement('P');
+          success.innerHTML = quote.value + ": quote as been added";
+          container.appendChild(success);
+     }
+     else{
+          var failed = createElement('P');
+          failed.innerHTML= quote.value + ": quote was previously added";
+          container.appendChild(failed);
+     }
+}
+
+start();
