41810dabb65a85f79a4799c99dbe3d3e91eb398c
Alisa Prusa <aprusa@ucsc.edu>
2015-11-04 17:06:08 -0800

    Fix Success Fail Notification

--- gameQuotes/gameQuotes.js
+++ gameQuotes/gameQuotes.js
@@ -2,18 +2,21 @@
 //https://parse.com/tutorials/todo-app-with-javascript
 //learn parse from code academy
 var container = document.getElementById('container');
+var textNode = document.createTextNode("Quote");
 
 void start(){
      var submit = documents.getElementsById('submit');
      var quote = documents.getElementsById('quote');
      if(quote.value == 'temp'){
-          var success = createElement('P');
-          success.innerHTML = quote.value + ": quote as been added";
+          var success = createElement('p');
+          textNode.textContent = quote.value + ": quote as been added";
+          success.appendChild(textNode);
           container.appendChild(success);
      }
      else{
-          var failed = createElement('P');
-          failed.innerHTML= quote.value + ": quote was previously added";
+          var failed = createElement('p');
+          textNode.textContent = quote.value + ": quote was previously added";
+          failed.appendChild(textNode);
           container.appendChild(failed);
      }
 }
